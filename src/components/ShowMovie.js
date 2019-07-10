59fe8b358d5e5344dca1bf8a550fc5694ca4db52
kod3180 <kod3180@gmail.com>
2019-07-10 00:00:18 +0000

    showMovie component styling initialized

--- src/components/ShowMovie.js
+++ src/components/ShowMovie.js
@@ -1,16 +1,44 @@
 import React from 'react';
 
 const ShowMovie = ({ movieInfo }) => {
-  // console.log(movieInfo);
-  let {Title, Year, Rated, Released} = movieInfo;
-  return(
-    <div>
-      <p>Title: {Title}</p>
-      <p>Year: {Year}</p>
-      <p>Rated: {Rated}</p>
-      <p>Released: {Released}</p>
+  function addDefault(e) {
+    e.target.src =
+      "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
+  }
+
+  let {
+    Title, 
+    Year, 
+    Rated, 
+    Released, 
+    Runtime, 
+    Genre,
+    Poster, 
+    Director,
+    Writer,
+    Actors,
+    Plot,
+    Awards,
+    imdbRating,
+    BoxOffice,
+    Production
+  } = movieInfo;
+  return (
+    <div className="container">
+      <div className="grid">
+        <div className="grid__image">
+          <img src={Poster} alt={Title} onError={addDefault} />
+        </div>
+        <div className="grid__content">
+          <h3>{Title} ({Year})</h3>
+          <p><b>Released:</b> {Released}</p>
+          <p><b>Runtime:</b> {Runtime}</p>
+          <p><b>Rated:</b> {Rated}</p>
+          <p><b>Genre:</b> {Genre}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ShowMovie;
